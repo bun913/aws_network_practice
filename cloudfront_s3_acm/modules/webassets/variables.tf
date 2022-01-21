5cbf714d35d07e14b7365120015aaa736a9fc7e5
bun913 <imanau03@gmail.com>
2022-01-21 17:53:42 +0900

    chore: tf fmtの漏れを修正

--- cloudfront_s3_acm/modules/webassets/variables.tf
+++ cloudfront_s3_acm/modules/webassets/variables.tf
@@ -1,8 +1,8 @@
 variable "bucket_list" {
-  type = list(map(string))
+  type        = list(map(string))
   description = "mapの要素 bucket_name,acl"
 }
 
 variable "tags" {
-  type = map
+  type = map(any)
 }
