e11ba9b7f9bd386b5cf6bd7ac19fd17ac3fec79e
bun913 <imanau03@gmail.com>
2022-01-23 11:30:54 +0900

    feat: 一旦過去の資産からECSタスクを作成するまでのmodulesを追加

---
+++ ecs_cloudwatch_logs/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+module "network" {
+  source          = "./modules/network/"
+  vpc_cidr        = var.vpc.cidr_block
+  private_subnets = var.private_subnets
+  alb_subnets     = var.alb_subnets
+  project         = var.project
+
+  tags = var.tags
+}
+
+module "web_app" {
+  source = "./modules/web_app/"
+
+  project     = var.project
+  alb_name    = "${var.project}-internal-lb"
+  vpc_id      = module.network.vpc_id
+  alb_subnets = module.network.alb_subnet_ids
+
+  vpc_cidr               = var.vpc.cidr_block
+  private_subnets        = module.network.private_subnet_ids
+  private_route_table_id = module.network.private_route_table_id
+  interface_services     = var.vpc_endpoint.interface
+  gateway_services       = var.vpc_endpoint.gateway
+
+  container_image_url = "${var.ecr_repo}:v1"
+  region              = var.region
+
+  tags = var.tags
+}
