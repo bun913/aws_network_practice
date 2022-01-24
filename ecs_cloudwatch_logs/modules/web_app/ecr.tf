c68aae8848993ef49efc2c8f16e913784aff85ae
bun913 <imanau03@gmail.com>
2022-01-25 08:25:18 +0900

    feat: firehose/cloudwatchlogsのresourceを追加

--- ecs_cloudwatch_logs/modules/web_app/ecr.tf
+++ ecs_cloudwatch_logs/modules/web_app/ecr.tf
@@ -8,7 +8,7 @@ resource "aws_ecr_repository" "app" {
 }
 
 # NOTE: アプリ側のリポジトリにimageをpushするためのスクリプトが用意されている
-resource "null_resource" "frontend" {
+resource "null_resource" "app" {
   triggers = {
     ecr_repo = aws_ecr_repository.app.name
   }
@@ -16,7 +16,7 @@ resource "null_resource" "frontend" {
   provisioner "local-exec" {
     # FIXME: アプリ側のリポジトリのファイルを指定しているため不恰好
     # 二重管理よりはマシだと思うがもっとスマートな方法はないか・・・
-    command = "sh ${path.root}/../../webapp-color/push_image.sh"
+    command = "cd ${path.root}/../../webapp-color && sh push_image.sh"
 
     // スクリプト専用の環境変数
     environment = {
---
+++ ecs_cloudwatch_logs/modules/web_app/log.tf
@@ -0,0 +1,43 @@
+# エラーログ配信用
+resource "aws_cloudwatch_log_group" "app" {
+  name              = "${var.project}-error-logs"
+  tags              = var.tags
+  retention_in_days = 90
+}
+
+# FireHose自体のロギング用
+resource "aws_cloudwatch_log_group" "firehose" {
+  name              = "${var.project}-firehose-logs"
+  tags              = var.tags
+  retention_in_days = 30
+}
+
+# デリバリーログ配信用
+resource "aws_kinesis_firehose_delivery_stream" "firelens" {
+  name        = "${var.project}-deliverystream"
+  destination = "s3"
+
+  s3_configuration {
+    role_arn   = aws_iam_role.firehose_role.arn
+    bucket_arn = aws_s3_bucket.app_logs.arn
+
+    cloudwatch_logging_options {
+      enabled         = "true"
+      log_group_name  = aws_cloudwatch_log_group.firehose.id
+      log_stream_name = "firehose_error"
+    }
+  }
+}
+
+# デリバリーログ長期保存用
+resource "aws_s3_bucket" "app_logs" {
+  acl = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
