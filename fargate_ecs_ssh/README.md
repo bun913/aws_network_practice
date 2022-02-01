248f6335fc4954720230f3a8478176097b5db406
bun913 <imanau03@gmail.com>
2022-02-02 06:41:05 +0900

    fix: 失敗した試みのフォルダを削除

--- fargate_ecs_ssh/README.md
+++
@@ -1,7 +0,0 @@
-# ECS Exec でFARGATE からsshトンネリングでDBに接続する
-
-システム構成図
-
-![構成図](system_archi.png)
-
-## 今回やること
--- fargate_ecs_ssh/modules/db/db_subnet_group.tf
+++
@@ -1,6 +0,0 @@
-# サブネットグループ for aurora
-resource "aws_db_subnet_group" "main" {
-  name       = var.subnet_group_name
-  subnet_ids = var.subnet_ids
-  tags       = var.tags
-}
